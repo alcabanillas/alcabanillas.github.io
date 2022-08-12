7cb3f5fa0e5eae094013cc8cd42f29e2edcbbdee
alcabanillas <acabanillas@gmail.com>
2022-08-12 08:51:11 +0200

    add format Prettier

--- js/routes.js
+++ js/routes.js
@@ -1,43 +1,41 @@
 (function () {
-    var queryString;
+  var queryString;
 
-    if (window.location.search != "") {
-        queryString = new window.URLSearchParams(window.location.search);
-        console.log(queryString);
+  if (window.location.search != "") {
+    queryString = new window.URLSearchParams(window.location.search);
+    console.log(queryString);
 
-        switch (queryString.get("d")) {
-            case "0":
-                $("#render").load("partials/home.html");
-                break;
-            case "1":
-                $("#render").load("partials/about.html");
-                break;
-            case "2":
-                $("#render").load("partials/portfolio.html");
-                break;
-            case "3":
-                $("#render").load("partials/contact.html");
-                break;
-        }
-    } else {
+    switch (queryString.get("d")) {
+      case "0":
         $("#render").load("partials/home.html");
+        break;
+      case "1":
+        $("#render").load("partials/about.html");
+        break;
+      case "2":
+        $("#render").load("partials/portfolio.html");
+        break;
+      case "3":
+        $("#render").load("partials/contact.html");
+        break;
     }
+  } else {
+    $("#render").load("partials/home.html");
+  }
 
-    // event handlers
-    $(document).on("click", ".goTo", goToClicked);
+  // event handlers
+  $(document).on("click", ".goTo", goToClicked);
 
-    function goToClicked(e) {
-        e.preventDefault();
+  function goToClicked(e) {
+    e.preventDefault();
 
-        partialView = e.currentTarget.dataset.partialview;
-        history.pushState(partialView, null, partialView);
-        loadPartialView(partialView);
-        e.stopPropagation();
-    }
+    partialView = e.currentTarget.dataset.partialview;
+    history.pushState(partialView, null, partialView);
+    loadPartialView(partialView);
+    e.stopPropagation();
+  }
 
-    function loadPartialView(partialView) {
-        $("#render").load("partials/" + partialView);
-    }
-
-    
+  function loadPartialView(partialView) {
+    $("#render").load("partials/" + partialView);
+  }
 })();
