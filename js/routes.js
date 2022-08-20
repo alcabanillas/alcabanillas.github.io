ca35a55965bfdb23bdca64f22e74552880e1a4ec
alcabanillas <acabanillas@gmail.com>
2022-08-20 14:46:20 +0200

    back-to-top button

--- js/routes.js
+++ js/routes.js
@@ -1,4 +1,5 @@
 (function () {
+  /* Redirect depending on the partial file being loaded */
   var queryString;
 
   if (window.location.search != "") {
@@ -26,9 +27,45 @@
     $("#render").load("partials/home.html");
   }
 
-  // event handlers
-  $(document).on("click", ".goTo", goToClicked);
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
 
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+
+
+  
+  /**
+   * Load partial view depending on click
+   */
+  
   function goToClicked(e) {
     e.preventDefault();
 
@@ -38,12 +75,34 @@
     e.stopPropagation();
     document.body.scrollTop = document.documentElement.scrollTop = 0;
   }
+  on('click', '.goTo', goToClicked, true);
 
   function loadPartialView(partialView) {
     $("#render").load("partials/" + partialView);
   }
 
+  /**
+   * Collapse navbar when clicked
+   */
   $(".navbar-collapse a").on("click", function () {
     $(".navbar-collapse").collapse("hide");
   });
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select(".back-to-top");
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add("active");
+      } else {
+        backtotop.classList.remove("active");
+      }
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+
+
 })();
