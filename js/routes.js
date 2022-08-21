dda0dbc385c350e81ec4e4424166750ac87d643f
alcabanillas <acabanillas@gmail.com>
2022-08-21 10:51:48 +0200

    routing

--- js/routes.js
+++ js/routes.js
@@ -1,22 +1,101 @@
 (function () {
   /* Redirect depending on the partial file being loaded */
-  var queryString;
+  var history = "";
 
-  const partials = ['home.html', 'about.html', 'resume.html', 'portfolio.html', 'contact.html'];
+  var routes = {
+    "": "partials/home.html",
+    "/": "partials/home.html",
+    "#/home": "partials/home.html",
+    "#/about": "partials/about.html",
+    "#/resume": "partials/resume.html",
+    "#/portfolio": "partials/portfolio.html",
+    "#/contact": "partials/contact.html"
+  };
 
-  let partial;
+ 
+  function router() {
+    var link = window.location.hash;
+    var innerElement = "";
 
-  if (window.location.search != "") {
-    queryString = new window.URLSearchParams(window.location.search);
-    partial = partials[queryString.get("d")];
+    // ----------------------------------------
+    // If more than one parameter in the link,
+    // I am targeting an element in the page,
+    // an anchor. First load page, the scroll
+    // the element into view.
+    // ----------------------------------------
+
+    var count = link.split("/").length - 1;
+    if (count > 1) {
+      // anchor element
+      innerElement = link.split("/")[2];
+
+      // page to load
+      link = "#/" + link.split("/")[1];
+    }
+
+    // ----------------------------------------
+    // Remember loaded page - used to avoid
+    // page reload on internal linking
+    // ----------------------------------------
+    if (history === link && innerElement) {
+      scrollIntoView(innerElement);
+      history = link;
+      return;
+    }
+    history = link;
+
+    // get path (route) for page
+    console.log("link: " + link);
+    var route = routes[link];
+
+    // if route exists, load page
+    console.log("route:" + route);
+    if (route) {
+      loadPage(route, innerElement);
+    }
+
+    setActiveItem(link);
   }
 
-  partial = partial || partials[0];
+  // listen hash path changes
+  window.addEventListener("hashchange", router);
 
-  $("#render").load(`partials/${partial}`);
+  // call the urlLocationHandler to load the page
+  router();
 
-  $(".navbar a.active").removeClass("active");
-  $(`.navbar a[data-partialview='${partial}']`).addClass("active");
+  async function loadPage(url, innerElement) {
+    // load page
+    const res = await fetch(url);
+    const content = await res.text();
+    const element = document.getElementById("render");
+    element.innerHTML = content;
+
+    // ------------------------------------------
+    // Scroll to top -- need to avoid navigation
+    // drift on page. Else the scroll state
+    // carries over on to next page
+    // ------------------------------------------
+    window.scrollTo(0, 0);
+
+    // element scroll into view
+    if (innerElement) {
+      scrollIntoView(innerElement);
+    }
+  }
+
+  function setActiveItem(link) {
+    $(".navbar a.active").removeClass("active");
+
+    if (link) {
+      $(`.navbar a[href='${link}']`).addClass("active");
+    } else {
+      $(".navbar a[href='#/home']").addClass("active");
+    }
+  }
+
+  function scrollIntoView(id) {
+    document.getElementById(id).scrollIntoView();
+  }
 
   /**
    * Easy selector helper function
@@ -58,25 +137,6 @@
     $(".navbar a.active").removeClass("active");
     $(this).addClass("active");
   });
-
-  /**
-   * Load partial view depending on click
-   */
-
-  function goToClicked(e) {
-    e.preventDefault();
-
-    partialView = e.currentTarget.dataset.partialview;
-    history.pushState(partialView, null, partialView);
-    loadPartialView(partialView);
-    //e.stopPropagation();
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
-  }
-  on('click', '.goTo', goToClicked, true);
-
-  function loadPartialView(partialView) {
-    $("#render").load("partials/" + partialView);
-  }
 
   /**
    * Collapse navbar when clicked
